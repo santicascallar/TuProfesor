5c59cfb20a237070f730a957d8e3df6a40157d53
santicascallar <101258956+santicascallar@users.noreply.github.com>
2022-05-20 12:05:58 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Alert, StyleSheet, Text, View, TouchableWithoutFeedback, TextInput } from 'react-native';
+import Main from './components/Main.jsx'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola Mundo!</Text>
-      <StatusBar style="auto" />
+    <View>
+     <Main />
     </View>
   );
 }
---
+++ components/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Alert, StyleSheet, Text, View, TouchableWithoutFeedback, TextInput } from 'react-native';
+
+const main = () => {
+    return (
+    <Text>Tu Profesor</Text>
+    )
+}
+
+export default main;
