05edfb188333e7bebedbefecae6cfdc465160a3e
santicascallar <101258956+santicascallar@users.noreply.github.com>
2022-06-03 04:32:45 -0300

    Add files via upload

---
+++ TuProfesor/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Alert, StyleSheet, Text, View, TouchableWithoutFeedback, TextInput } from 'react-native';
+
+const main = () => {
+    return (
+        <View>
+            <Text>Tu Profesor</Text>
+        </View>
+    )
+}
+
+export default main;
