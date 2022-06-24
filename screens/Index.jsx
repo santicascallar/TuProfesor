8253c968812826a5df87cf47d377cebdf102f777
santicascallar <101258956+santicascallar@users.noreply.github.com>
2022-06-24 12:14:05 -0300

    Add files via upload

--- screens/Index.jsx
+++ screens/Index.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 import {StyleSheet, Text, View, FlatList } from 'react-native';
 
 const Index = () => {
+
+    const [profesores, setProfesores] = useState([]);
+
+    useEffect(() =>{
+        axios.get("https://localhost:5000/profesores")
+        .then(res => {
+            console.log(res.data);
+            setProfesores(res.data);
+        })
+    })
+
     return (
         <View>
             <Text>Lista de Profesores</Text>
             <FlatList
             />
+            <ContactosFlatlist contactos={contactos} verDetalles={verDetalles}/>
         </View>
     )
 }
