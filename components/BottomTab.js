0ac600922ec6cf76a81c74b7b2f03d0f64c48e0a
santicascallar <101258956+santicascallar@users.noreply.github.com>
2022-08-10 10:21:07 -0300

    Update BottomTab.js

--- components/BottomTab.js
+++ components/BottomTab.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //screens
 import Home from "../screens/Home";
